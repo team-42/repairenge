4e9811056ec79fac28e4b42d0e045dd651bac6e0
Merando <merando@gmx.de>
2020-02-01 22:41:03 +0100

    added jet component

---
+++ ship_components/ship_component_jet.py
@@ -0,0 +1,21 @@
+from constants import Resources, BatchNames
+from ship_components import ship_component
+import globals
+
+
+class Jet(ship_component.ShipComponent):
+    def __init__(self, x, y, owner, *args, **kwargs):
+        super(Jet, self).__init__(x, y, owner,
+                                          globals.resources[Resources.Image_Ship_Module_Base],
+                                          batch=globals.sprite_batches[BatchNames.Component_Batch],
+                                          *args, *kwargs)
+
+        self.engine_power = 100000
+        self.mass = 50
+
+    def module_initial(self, ship):
+        ship.mass += self.mass
+        ship.engine_power += self.engine_power
+
+    def update(self, dt):
+        super(Jet, self).update(dt)
