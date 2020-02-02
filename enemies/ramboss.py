56541b01259ec22b9f8dcfb211db12654b4cae7b
Skamandros <misc@skamandros.de>
2020-02-02 13:44:09 +0100

    Polished Ramboss

--- enemies/ramboss.py
+++ enemies/ramboss.py
@@ -7,6 +7,7 @@ from enemies.enemy import StoryEnemy
 from ship_components.ship_component_heavy_hull import HeavyHull
 from ship_components.ship_component_jet import Jet
 from ship_components.ship_component_ram import Ram
+from ship_components.ship_component_shield import Shield
 
 RAM_IDLE = 0
 RAM_FORWARD = 1
@@ -36,11 +37,11 @@ class RamBoss(StoryEnemy):
         self.upgrade(Ram(1, 3, self))
 
         self.upgrade(HeavyHull(-1, -1, self))
-        self.upgrade(HeavyHull(0, -1, self))
         self.upgrade(HeavyHull(-1, 1, self))
-        self.upgrade(HeavyHull(0, 1, self))
         self.upgrade(HeavyHull(1, -2, self))
         self.upgrade(HeavyHull(1, 2, self))
+        self.upgrade(Shield(0, -1, self))
+        self.upgrade(Shield(0, 1, self))
 
         self.upgrade(Jet(3, -2, self))
         self.upgrade(Jet(2, -2, self))
@@ -64,13 +65,14 @@ class RamBoss(StoryEnemy):
             if self.x > globals.window.width * 0.85:
                 self.ram_attack = RAM_IDLE
                 self.engine_power /= RAMMING_SPEED
-        elif self.time > 5 and random.random() < RAM_PROBABILITY + (1 - self.get_health_percentage() / 100):
+        elif self.time > 5 and random.random() < RAM_PROBABILITY + (
+                RAM_PROBABILITY - RAM_PROBABILITY * self.get_health_percentage() / 100):
             self.ram_attack = RAM_FORWARD
             self.engine_power *= RAMMING_SPEED
         else:
             self.time += dt
             # fly up and down
-            self.y += math.sin(self.time) * self.engine_power * dt / (self.mass * 1.8)
+            self.y += math.sin(self.time) * self.engine_power * dt / (self.mass * 2.4)
             # fly to the left
             if self.x > globals.window.width * 0.85:
                 self.x -= 0.5 * self.engine_power / self.mass * dt
